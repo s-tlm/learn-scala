8b40ddd6fa3ea767aa8a4b13a3808ed1cc36586a
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-09-09 13:01:47 +1000

    Complete exercises

--- my-project/src/main/scala/lectures.part2oop/MethodNotations.scala
+++ my-project/src/main/scala/lectures.part2oop/MethodNotations.scala
@@ -2,7 +2,7 @@ package lectures.part2oop
 
 object MethodNotations extends App {
   
-  class Person(val name: String, favouriteMovie: String) {
+  class Person(val name: String, favouriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favouriteMovie 
 
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}!"
@@ -14,6 +14,20 @@ object MethodNotations extends App {
 
     // Need brackets "()"
     def apply(): String = s"Hi, my name is ${name} and I like ${favouriteMovie}."
+
+    // Exercises
+    // 1.
+    def +(nicname: String): Person = new Person(s"${name} (${nicname})", favouriteMovie)
+
+    // 2.
+    def unary_+ : Person = new Person(name, favouriteMovie, age + 1)
+
+    // 3.
+    def learns(subject: String): String = s"${name} learns ${subject}."
+    def learnsScala: String = this learns "Scala"
+
+    // 4.
+    def apply(n: Int): String = s"${name} watched ${favouriteMovie} ${n} times!"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -48,16 +62,25 @@ object MethodNotations extends App {
   // apply
   println(mary.apply()) 
   println(mary()) // equivalent. Looks for a method called apply() in the class
+  // no need to specify the method name! Can specify any method called apply
+  // with or without input variables.
 
   // Exercises
   // 1. Overload the + operator. Receives a string and returns a new Person with a nickname
   //  mary + "the rockstar" => new person "Mary (the rockstar)"
+  
   // 2. Add an age method to Person class. 
   //  Add unary + operator => new Person with age + 1
+  println((+mary).age)
+
   // 3. Add a learns method in the Person class
   //  mary learns "scala" => "Mary learns scala"
   //  Add a learnScala method, calls learns method with "Scala"
   //  Use postfix notation.
+  println(mary learns "maths")
+  println(mary learnsScala)
+
   // 4. Overload apply method
   //  mary.apply(2) => "Mary watched Inception 2 times."
+  println(mary(2))
 }
