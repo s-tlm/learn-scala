145fd793d8269dc4ecb0a9741769739760b93fc3
Steven Lam <slam@Stevens-MacBook-Air.local>
2023-11-20 19:47:49 +1100

    Add AnonymousClasses.scala

---
+++ my-project/src/main/scala/lectures.part2oop/AnonymousClasses.scala
@@ -0,0 +1,11 @@
+package lectures.part2oop
+
+object AnonymousClasses extends App {
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("hahahahahah")
+  }
+}
